c4e1826334055cf7650fb81c64d412bed92ac8cb
Tasnia <164462424+Tasnia22@users.noreply.github.com>
2024-05-26 21:07:45 +0600

    How to drive a servo

---
+++ servoDrive.c.ino
@@ -0,0 +1,26 @@
+#include<Servo.h>
+Servo myservo;
+int pos =0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  myservo.attach(9);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  {
+  for(pos=0;pos<=270;pos+=1)
+  myservo.write(pos);
+  delay(15);
+  }
+  for(pos=270;pos>=0;pos-=1)
+  {
+    myservo.write(pos);
+    delay(15);
+  }
+}
