0662c371be090ef9b4af967da98f32ebf06eb67b
Tasnia <164462424+Tasnia22@users.noreply.github.com>
2024-05-26 21:11:35 +0600

    servoDrive.c.ino

--- servoDrive.c.ino
+++ servoDrive.c.ino
@@ -2,19 +2,14 @@
 Servo myservo;
 int pos =0;
 
-
 void setup() {
-  // put your setup code here, to run once:
 
   myservo.attach(9);
-
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  {
   for(pos=0;pos<=270;pos+=1)
+    {
   myservo.write(pos);
   delay(15);
   }
